0aa3f30493a3dd0c9c808bc538b767e15dd367f0
Violeta Rodriguez <vl.rodriguez10@uniandes.edu.co>
2019-08-21 20:12:10 -0500

    Clase persitence par ServicioOfrecido lista

--- s4_servicios-back/src/main/java/co/edu/uniandes/csw/servicios/persistence/ServicioOfrecidoPersistence.java
+++ s4_servicios-back/src/main/java/co/edu/uniandes/csw/servicios/persistence/ServicioOfrecidoPersistence.java
@@ -5,29 +5,39 @@
  */
 package co.edu.uniandes.csw.servicios.persistence;
 
+import co.edu.uniandes.csw.servicios.entities.ServicioOfrecidoEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.Id;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
  * @author Violeta Rodríguez
  */
-@Stateless 
-public class ServicioOfrecidoPersistence 
-{
-@PersistenceContext(unitName="serviciosPU")    
-    @Id
-protected EntityManager em;
+@Stateless
+public class ServicioOfrecidoPersistence {
 
-    public EntityManager getEm() {
-        return em;
+    @PersistenceContext(unitName = "serviciosPU")
+    protected EntityManager em;
+
+    public ServicioOfrecidoEntity create(ServicioOfrecidoEntity servicioOfrecido) {
+        em.persist(servicioOfrecido);
+        return servicioOfrecido;
+
     }
 
-    public void setEm(EntityManager em) {
-        this.em = em;
+    public ServicioOfrecidoEntity find(Long servicioOfrecidoId) {
+        return em.find(ServicioOfrecidoEntity.class, servicioOfrecidoId);
     }
 
+    public List<ServicioOfrecidoEntity> findAll() {
+        
+        TypedQuery<ServicioOfrecidoEntity> query= em.createQuery("select u from ServicioOfrecidoEntity u", ServicioOfrecidoEntity.class);
+        
+        return query.getResultList();
+    }
 }
