dff6f5bc06901087b44225890a8d6c98f8fa8788
santiagocala <s.cala@uniandes.edu.co>
2019-08-26 11:45:32 -0500

    Las clases ClienteEntity y ClientePersistence se comentaron

--- s4_servicios-back/src/main/java/co/edu/uniandes/csw/servicios/entities/ClienteEntity.java
+++ s4_servicios-back/src/main/java/co/edu/uniandes/csw/servicios/entities/ClienteEntity.java
@@ -5,7 +5,7 @@
  */
 package co.edu.uniandes.csw.servicios.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  *
@@ -15,49 +15,100 @@ import javax.persistence.Entity;
 @Entity            
 public class ClienteEntity extends BaseEntity {
     
-    private int id;
+    @Id
+    private Long id;
     private String nombre; 
     private int telefono;
     private String mail;
     private String usuario;
     private String contrasena;
     
+    @Override
+    public Long getId(){
+        return id;
+    }
+    
+    @Override
+    public void setId(Long pId){
+        id = pId;
+    }
+    
+    /**
+     * Retorna el nombre del cliente
+     * @return el nombre
+     */
     public String getNombre(){
         return nombre;
     }
     
+    /**
+     * Cambia el nombre del cliente por uno dado por parámetro
+     * @param pNombre - El nuevo nombre del cliente
+     */
     public void setNombre(String pNombre){
         nombre = pNombre;
     }
     
+    /**
+     * Retorna número de teléfono del cliente
+     * @return el teléfono
+     */
     public int getTelefono(){
         return telefono;
     }
     
+    /**
+     * Cambia el número de teléfono del cliente por uno dado por parámetro
+     * @param pTelefono - El nuevo teléfono
+     */
     public void setTelefono(int pTelefono){
         telefono = pTelefono;
     }
     
+    /**
+     * Retorna el correo electrónico del cliente
+     * @return el email
+     */
     public String getMail(){
         return mail;
     }
     
+    /**
+     * Cambia el correo electrónico del cliente por uno dado por parámetro
+     * @param pMail - el nuevo correo del cliente
+     */
     public void setMail(String pMail){
         mail = pMail;
     }
     
+    /**
+     * Retorna el usuario del cliente
+     * @return - el usuario
+     */
     public String getUsuario(){
         return usuario;
     }
     
+    /**
+     * Cambia el usuario del cliente por uno dado por parámetro
+     * @param pUsuario - el nuevo usuario del cliente
+     */
     public void setUsuario(String pUsuario){
         usuario = pUsuario;
     }
     
+    /**
+     * Retorna la contraseña del usuario
+     * @return  - la contraseña
+     */
     public String getContrasena(){
         return contrasena;
     }
     
+    /**
+     * Cambia la contraseña del cliente por una dada por parámetro
+     * @param pContrasena . la nueva contraseña
+     */
     public void setContrasena(String pContrasena){
         contrasena = pContrasena;
     }
