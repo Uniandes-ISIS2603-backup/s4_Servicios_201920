376cc83c4b66d711f687c800353b8ae0e095c1c8
VioletaLuna-Uniandes <vl.rodriguez10@uniandes.edu.co>
2019-09-18 16:02:03 -0500

    ResporteMesnsualDTO ok

--- s4_servicios-api/src/main/java/co/edu/uniandes/csw/servicios/dtos/ReporteMensualDTO.java
+++ s4_servicios-api/src/main/java/co/edu/uniandes/csw/servicios/dtos/ReporteMensualDTO.java
@@ -5,6 +5,11 @@
  */
 package co.edu.uniandes.csw.servicios.dtos;
 
+import co.edu.uniandes.csw.servicios.adapters.DateAdapter;
+import co.edu.uniandes.csw.servicios.entities.ReporteMensualEntity;
+
+import java.util.Date;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
  *
@@ -12,11 +17,97 @@ package co.edu.uniandes.csw.servicios.dtos;
  */
 public class ReporteMensualDTO 
 {
+   /**
+     * Mes al que corresponde el reporte
+     */
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    private Date mes;
     
+    /**
+     * Ingresos que serecibieron en el mes
+     */
+    private double ingresos;
+    
+    /**
+     * Gasto que se generaron en el mes.
+     */
+    private double egresos;
+    
+    /**
+     * Número de servicios prestados en el mes.
+     */
+    private int numSerivico;
 
-    public ReporteMensualDTO() {
+    private Long id;
+
+
+    public ReporteMensualDTO()
+    {
+        
     }
     
+     public ReporteMensualDTO(ReporteMensualEntity entity)
+    {
+        this.egresos=entity.getEgresos();
+        this.id=entity.getId();
+        this.ingresos=entity.getIngresos();
+        this.numSerivico= entity.getNumSerivico();
+        this.mes= entity.getMes();
+               
+    }
+     
+     public ReporteMensualEntity toEntity()
+     {
+         ReporteMensualEntity entity = new ReporteMensualEntity();
+         
+         entity.setEgresos(this.egresos);
+         entity.setIngresos(this.ingresos);
+         entity.setId(this.id);
+         entity.setMes(this.mes);
+         entity.setNumSerivico(this.numSerivico);
+         
+         return entity;
+     }
+    
+    public Date getMes() {
+        return mes;
+    }
+
+    public void setMes(Date mes) {
+        this.mes = mes;
+    }
+
+    public double getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(double ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public double getEgresos() {
+        return egresos;
+    }
+
+    public void setEgresos(double egresos) {
+        this.egresos = egresos;
+    }
+
+    public int getNumSerivico() {
+        return numSerivico;
+    }
+
+    public void setNumSerivico(int numSerivico) {
+        this.numSerivico = numSerivico;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
      
     
     
