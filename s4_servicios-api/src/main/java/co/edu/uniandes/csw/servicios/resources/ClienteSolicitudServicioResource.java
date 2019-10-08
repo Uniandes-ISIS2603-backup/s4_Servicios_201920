aac2530fc7cb50c5b9fda5d02d0d2fe40fa5fe88
santiagocala <s.cala@uniandes.edu.co>
2019-10-08 16:23:12 -0500

    Ya se agregaron correctamente todos los métodos a la clase ClienteSSResource, pero faltan los delete

--- s4_servicios-api/src/main/java/co/edu/uniandes/csw/servicios/resources/ClienteSolicitudServicioResource.java
+++ s4_servicios-api/src/main/java/co/edu/uniandes/csw/servicios/resources/ClienteSolicitudServicioResource.java
@@ -1,6 +1,9 @@
 package co.edu.uniandes.csw.servicios.resources;
 
+import co.edu.uniandes.csw.servicios.dtos.SolicitudServicioDTO;
 import co.edu.uniandes.csw.servicios.ejb.ClienteSolicitudServicioLogic;
+import co.edu.uniandes.csw.servicios.ejb.SolicitudServicioLogic;
+import co.edu.uniandes.csw.servicios.entities.SolicitudServicioEntity;
 import co.edu.uniandes.csw.servicios.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.servicios.mappers.WebApplicationExceptionMapper;
 import java.util.List;
@@ -48,84 +51,52 @@ public class ClienteSolicitudServicioResource {
     @GET
     public List<SolicitudServicioDTO> getServicios(@PathParam("clientesId") Long clientesId) {
         LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource getServicios: input: {0}", clientesId);
-        List<SolicitudServicioDTO> lista = serviciosListEntity2DTO(authorBookLogic.getBooks(authorsId));
-        LOGGER.log(Level.INFO, "AuthorBooksResource getBooks: output: {0}", lista);
+        List<SolicitudServicioDTO> lista = serviciosListEntity2DTO(clienteSolicitudServicioLogic.getServicios(clientesId));
+        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource getServicios: output: {0}", lista);
         return lista;
     }
 
-    /**
-     * Busca y devuelve el libro con el ID recibido en la URL, relativo a un
-     * autor.
-     *
-     * @param authorsId El ID del autor del cual se busca el libro
-     * @param booksId El ID del libro que se busca
-     * @return {@link BookDetailDTO} - El libro encontrado en el autor.
-     * @throws co.edu.uniandes.csw.bookstore.exceptions.BusinessLogicException
-     * si el libro no está asociado al autor
-     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
-     */
     @GET
-    @Path("{booksId: \\d+}")
-    public BookDetailDTO getBook(@PathParam("authorsId") Long authorsId, @PathParam("booksId") Long booksId) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "AuthorBooksResource getBook: input: authorsId {0} , booksId {1}", new Object[]{authorsId, booksId});
-        if (bookLogic.getBook(booksId) == null) {
-            throw new WebApplicationException("El recurso /books/" + booksId + " no existe.", 404);
+    @Path("{serviciosId: \\d+}")
+    public SolicitudServicioDTO getServicio(@PathParam("clientesId") Long clientesId, @PathParam("serviciosId") Long serviciosId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource getServicio: input: authorsId {0} , booksId {1}", new Object[]{clientesId, serviciosId});
+        if (solicitudServicioLogic.getSolicitudServicio(serviciosId) == null) {
+            throw new WebApplicationException("El recurso /servicios/" + serviciosId + " no existe.", 404);
         }
-        BookDetailDTO detailDTO = new BookDetailDTO(authorBookLogic.getBook(authorsId, booksId));
-        LOGGER.log(Level.INFO, "AuthorBooksResource getBook: output: {0}", detailDTO);
-        return detailDTO;
+        SolicitudServicioDTO servicioDTO = new SolicitudServicioDTO(clienteSolicitudServicioLogic.getSolicitudServicio(clientesId, serviciosId));
+        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource getServicio: output: {0}", servicioDTO);
+        return servicioDTO;
     }
 
-    /**
-     * Actualiza la lista de libros de un autor con la lista que se recibe en el
-     * cuerpo
-     *
-     * @param authorsId El ID del autor al cual se le va a asociar el libro
-     * @param books JSONArray {@link BookDetailDTO} - La lista de libros que se
-     * desea guardar.
-     * @return JSONArray {@link BookDetailDTO} - La lista actualizada.
-     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
-     */
+    
     @PUT
-    public List<BookDetailDTO> replaceBooks(@PathParam("authorsId") Long authorsId, List<BookDetailDTO> books) {
-        LOGGER.log(Level.INFO, "AuthorBooksResource replaceBooks: input: authorsId {0} , books {1}", new Object[]{authorsId, books});
-        for (BookDetailDTO book : books) {
-            if (bookLogic.getBook(book.getId()) == null) {
-                throw new WebApplicationException("El recurso /books/" + book.getId() + " no existe.", 404);
+    public List<SolicitudServicioDTO> replaceSolicitudesServicio(@PathParam("clientesId") Long clientesId, List<SolicitudServicioDTO> solicitudesServicios) {
+        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource replaceSolicitudesServicio: input: clientesId {0} , solicitudesServicios {1}", new Object[]{clientesId, solicitudesServicios});
+        for (SolicitudServicioDTO servicio : solicitudesServicios) {
+            if (solicitudServicioLogic.getSolicitudServicio(servicio.getId()) == null) {
+                throw new WebApplicationException("El recurso /servicios/" + servicio.getId() + " no existe.", 404);
             }
         }
-        List<BookDetailDTO> lista = booksListEntity2DTO(authorBookLogic.replaceBooks(authorsId, booksListDTO2Entity(books)));
-        LOGGER.log(Level.INFO, "AuthorBooksResource replaceBooks: output: {0}", lista);
+        List<SolicitudServicioDTO> lista = serviciosListEntity2DTO(clienteSolicitudServicioLogic.replaceSolicitudServicios(clientesId, serviciosListDTO2Entity(solicitudesServicios)));
+        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource replaceSolicitudesServicio: output: {0}", lista);
         return lista;
     }
 
-    /**
-     * Elimina la conexión entre el libro y e autor recibidos en la URL.
-     *
-     * @param authorsId El ID del autor al cual se le va a desasociar el libro
-     * @param booksId El ID del libro que se desasocia
-     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
-     */
-    @DELETE
-    @Path("{booksId: \\d+}")
-    public void removeBook(@PathParam("authorsId") Long authorsId, @PathParam("booksId") Long booksId) {
-        LOGGER.log(Level.INFO, "AuthorBooksResource deleteBook: input: authorsId {0} , booksId {1}", new Object[]{authorsId, booksId});
-        if (bookLogic.getBook(booksId) == null) {
-            throw new WebApplicationException("El recurso /books/" + booksId + " no existe.", 404);
-        }
-        authorBookLogic.removeBook(authorsId, booksId);
-        LOGGER.info("AuthorBooksResource deleteBook: output: void");
-    }
+//    /*
+//     * Elimina la conexión entre el servicio y el cliente recibidos en la URL.
+//     */
+//    @DELETE
+//    @Path("{serviciosId: \\d+}")
+//    public void removeSolicitudServicio(@PathParam("clientesId") Long clientesId, @PathParam("serviciosId") Long serviciosId) {
+//        LOGGER.log(Level.INFO, "ClienteSolicitudServicioResource deleteServicio: input: clientesId {0} , serviciosId {1}", new Object[]{clientesId, serviciosId});
+//        if (solicitudServicioLogic.getSolicitudServicio(serviciosId) == null) {
+//            throw new WebApplicationException("El recurso /servicios/" + serviciosId + " no existe.", 404);
+//        }
+//        clienteSolicitudServicioLogic.removeSolicitudServicio(clientesId, serviciosId);
+//        LOGGER.info("AuthorBooksResource deleteBook: output: void");
+//    }
 
-    /**
-     * Convierte una lista de SolicitudServicioEntity a una lista de SolicitudServicioDTO.
-     *
-     * @param entityList Lista de SolicitudServicioEntity a convertir.
-     * @return Lista de SolicitudServicioDTO convertida.
-     */
+
     private List<SolicitudServicioDTO> serviciosListEntity2DTO(List<SolicitudServicioEntity> entityList) {
         List<SolicitudServicioDTO> list = new ArrayList<>();
         for (SolicitudServicioEntity entity : entityList) {
@@ -134,15 +105,10 @@ public class ClienteSolicitudServicioResource {
         return list;
     }
 
-    /**
-     * Convierte una lista de BookDetailDTO a una lista de BookEntity.
-     *
-     * @param dtos Lista de BookDetailDTO a convertir.
-     * @return Lista de BookEntity convertida.
-     */
-    private List<BookEntity> booksListDTO2Entity(List<BookDetailDTO> dtos) {
-        List<BookEntity> list = new ArrayList<>();
-        for (BookDetailDTO dto : dtos) {
+   
+    private List<SolicitudServicioEntity> serviciosListDTO2Entity(List<SolicitudServicioDTO> dtos) {
+        List<SolicitudServicioEntity> list = new ArrayList<>();
+        for (SolicitudServicioDTO dto : dtos) {
             list.add(dto.toEntity());
         }
         return list;
