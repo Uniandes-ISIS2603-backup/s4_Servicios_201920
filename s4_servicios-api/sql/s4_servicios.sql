2e035679681850a10c9d5712c77ab11a9b32bab3
Juan Lucas <jl.ibarra@uniandes.edu.co>
2019-10-10 16:41:16 -0500

    Pruebas Postman

--- s4_servicios-api/sql/s4_servicios.sql
+++ s4_servicios-api/sql/s4_servicios.sql
@@ -1,1 +1,4 @@
-delete from ClienteEntity
+delete from ClienteEntity;
+delete from FacturaEntity;
+
+insert into FacturaEntity (id, duracion, preciomateriales, fecha, pagada, primerpago, solicitud_id, tarjetapago_id) values (10, 10, 10, '8/20/1996', 0, 0, 1, null);
